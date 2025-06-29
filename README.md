fca5281954687de6411e430b834520ca247efd88
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-06-29 19:37:38 +0300

    WIP

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lox
+
+Lox is a full-featured, efficient scripting language from Robert Nystrom's book, [Crafting Interpreters](https://craftinginterpreters.com/). 
+
+In Jlox, the Java implementation of Lox, scripts are parsed using a recursive descent parser then interpreted by traversing the generated AST.
---
+++ jlox/src/main/java/dev/muhings/lox/Lox.java
@@ -0,0 +1,18 @@
+package dev.muhings.lox;
+
+import java.io.IOException;
+
+public class Lox {
+	 public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64); 
+    } else if (args.length == 1) {
+      // runFile(args[0]);
+			System.out.println("Not yet implemented - runFile");
+    } else {
+			// runPrompt();
+      System.out.println("Not yet implemented - runPrompt");
+    }
+  }
+}
