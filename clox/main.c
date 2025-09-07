c34848a9e9d85e6e6185c3354829898358c69c5c
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-09-08 00:58:26 +0300

    basic operations with byte code

--- clox/main.c
+++ clox/main.c
@@ -1,5 +1,9 @@
 #include "common.h"
+#include "chunk.h"
 
 int main(int argc, const char* argv[]) {
-  return 0;
+  Chunk chunk;
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+  freeChunk(&chunk);
 }
