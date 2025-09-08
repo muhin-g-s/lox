8b7e4bd52e3b35eae56189e9e1d89aa509058584
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-09-09 00:29:55 +0300

    add flag to print trace vm

--- clox/main.c
+++ clox/main.c
@@ -15,7 +15,7 @@ int main(int argc, const char* argv[]) {
 
   writeChunk(&chunk, OP_RETURN, 1);
 
-	disassembleChunk(&chunk, "test chunk");
+	// disassembleChunk(&chunk, "test chunk");
 
 	interpret(&chunk);
 	
--- clox/vm.c
+++ clox/vm.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "vm.h"
+#include "debug.h"
 #include <stdio.h>
 
 VM vm; 
@@ -15,6 +16,10 @@ static InterpretResult run() {
 	#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 
 	for (;;) {
+		#ifdef DEBUG_TRACE_EXECUTION
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+		#endif
+
 		uint8_t instruction;
 		switch (instruction = READ_BYTE()) {
 			case OP_CONSTANT: {
