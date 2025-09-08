91e9495c59b980fedd35db3f4ebb4c59a427247f
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-09-08 22:45:02 +0300

    add line info

--- clox/chunk.h
+++ clox/chunk.h
@@ -14,11 +14,12 @@ typedef struct {
   int capacity;
   uint8_t* code;
 	ValueArray constants;
+	int* lines;
 } Chunk;
 
 void initChunk ( Chunk * chunk );
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 
 #endif
