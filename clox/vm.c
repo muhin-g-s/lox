d3e7108db9de288d5b02b33270cfe754355dba0c
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-09-09 00:43:55 +0300

    add stack vm

--- clox/vm.c
+++ clox/vm.c
@@ -5,7 +5,12 @@
 
 VM vm; 
 
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
 void initVM() {
+	resetStack();
 }
 
 void freeVM() {
@@ -42,4 +47,14 @@ InterpretResult interpret(Chunk* chunk) {
   vm.chunk = chunk;
   vm.ip = vm.chunk->code;
   return run();
+}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
 }
