d5a002d5c0f0f0539a6039e931a812fc075041d5
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-09-09 21:55:40 +0300

    unary minus instruction

--- clox/main.c
+++ clox/main.c
@@ -10,12 +10,13 @@ int main(int argc, const char* argv[]) {
   initChunk(&chunk);
 
 	int  constant = addConstant (& chunk , 1.2 );
+	
 	writeChunk(&chunk, OP_CONSTANT, 1);
 	writeChunk(& chunk, constant, 1);
+	writeChunk(&chunk, OP_NEGATE, 123);
 
   writeChunk(&chunk, OP_RETURN, 1);
 
-	// disassembleChunk(&chunk, "test chunk");
 
 	interpret(&chunk);
 	
