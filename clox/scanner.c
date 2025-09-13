3b4bd980c271004859c90778f4eec20e31162e8c
Мухин Григорий <muhin.g.s.00@gmail.com>
2025-09-13 16:12:32 +0300

    single and one or two character tokens

--- clox/scanner.c
+++ clox/scanner.c
@@ -19,6 +19,9 @@ void initScanner(const char* source) {
 }
 
 bool isAtEnd();
+char advance();
+bool match(char expected);
+
 Token makeToken(TokenType type);
 Token errorToken(const char* message);
 
@@ -27,11 +30,52 @@ Token scanToken() {
 
   if (isAtEnd()) return makeToken(TOKEN_EOF);
 
+	char c = advance();
+
+  switch (c) {
+    case '(': return makeToken(TOKEN_LEFT_PAREN);
+    case ')': return makeToken(TOKEN_RIGHT_PAREN);
+    case '{': return makeToken(TOKEN_LEFT_BRACE);
+    case '}': return makeToken(TOKEN_RIGHT_BRACE);
+    case ';': return makeToken(TOKEN_SEMICOLON);
+    case ',': return makeToken(TOKEN_COMMA);
+    case '.': return makeToken(TOKEN_DOT);
+    case '-': return makeToken(TOKEN_MINUS);
+    case '+': return makeToken(TOKEN_PLUS);
+    case '/': return makeToken(TOKEN_SLASH);
+    case '*': return makeToken(TOKEN_STAR);
+
+		case '!':
+      return makeToken(
+          match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+    case '=':
+      return makeToken(
+          match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+    case '<':
+      return makeToken(
+          match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+    case '>':
+      return makeToken(
+          match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+  }
+
   return errorToken("Unexpected character.");
 }
 
 bool isAtEnd() {
   return *scanner.current == '\0';
+}
+
+char advance() {
+  scanner.current++;
+  return scanner.current[-1];
+}
+
+bool match(char expected) {
+  if (isAtEnd()) return false;
+  if (*scanner.current != expected) return false;
+  scanner.current++;
+  return true;
 }
 
 Token makeToken(TokenType type) {
