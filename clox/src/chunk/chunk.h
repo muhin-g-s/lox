d4a6cc35c73a6683f6046ab7f1d73682f51d4924
Мухин Григорий <muhin.g.s.00@gmail.com>
2026-03-07 21:45:48 +0300

    fix

--- clox/src/chunk/chunk.h
+++ clox/src/chunk/chunk.h
@@ -22,8 +22,9 @@ typedef struct {
 	int* lines;
 } Chunk;
 
-void initChunk ( Chunk * chunk );
+void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
+
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 
